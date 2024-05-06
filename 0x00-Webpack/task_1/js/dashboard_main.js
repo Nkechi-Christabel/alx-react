8a0d27fbb5a8024490ed3048a2fd0692d6efd96d
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-06 14:04:02 +0100

    Updated the counter function.

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,15 @@
 import $ from "jquery";
 import _ from "lodash";
 
-$(document).ready(function () {
-  // Elements
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<button>Click here to get started</button>");
-  $("body").append('<p id="count"></p>');
-  $("body").append("<p>Copyright - Holberton School</p>");
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
+$("body").append("<p>Copyright - Holberton School</p>");
 
-  // Counter function
-  let count = 0;
-  const updateCounter = _.debounce(() => {
-    count++;
-    $("#count").text(`${count} clicks on the button`);
-  }, 300);
+let conunt = 0;
 
-  // Button click event
-  $("button").on("click", updateCounter);
-});
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+}
