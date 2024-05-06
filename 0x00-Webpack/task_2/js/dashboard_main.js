4bfbd2660ce7a8658dbc9009e267806040a761ff
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-06 15:44:35 +0100

    Added CSS and Images configuration.

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import "../css/main.css";
+import $ from "jquery";
+import _ from "lodash";
+
+$(function () {
+  // Elements
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<button>Click here to get started</button>");
+  $("body").append('<p id="count"></p>');
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  // Counter function
+  let count = 0;
+  const updateCounter = _.debounce(() => {
+    count++;
+    $("#count").text(`${count} clicks on the button`);
+  }, 500);
+
+  // Button click event
+  $("button").on("click", updateCounter);
+});
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets",
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
