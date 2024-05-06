23e042181f81ab01d2ab051e8ee4910e4e5bbf89
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-06 12:52:10 +0100

    Created Webpack with a config file.

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from "jquery";
+import _ from "lodash";
+
+$(document).ready(function () {
+  // Elements
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append(
+    '<button id="startButton">Click here to get started</button>'
+  );
+  $("body").append('<p id="count"></p>');
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  // Counter function
+  let count = 0;
+  const updateCounter = _.debounce(() => {
+    count++;
+    $("#count").text(`${count} clicks on the button`);
+  }, 300);
+
+  // Button click event
+  $("#startButton").click(updateCounter);
+});
