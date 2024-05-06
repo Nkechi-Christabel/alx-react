2af40de16d6cbc9ad20bda515556e6ae6933bb11
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-06 23:37:59 +0100

    Updated counter function in task 2

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -4,7 +4,7 @@ import _ from "lodash";
 
 $(function () {
   // Elements
-  $("body").append("<div id='logo'></div>");
+  $("body").append('<div id="logo"></div>');
   $("body").append("<p>Holberton Dashboard</p>");
   $("body").append("<p>Dashboard data for the students</p>");
   $("body").append("<button>Click here to get started</button>");
