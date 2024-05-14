2746a016f5f95cab72886e824a4ce69df1705cb2
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-14 21:24:47 +0100

    Updated the navigation file.

--- 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -6,7 +6,10 @@ function NotificationItem({ type, html, value }) {
     <>
       {type && value ? <li data-notification-type={type}>{value}</li> : null}
       {html ? (
-        <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+        <li
+          data-notification-type={type}
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></li>
       ) : null}
     </>
   );
