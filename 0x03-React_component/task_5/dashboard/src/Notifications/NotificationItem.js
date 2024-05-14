1f788874d97c5c303735496555cc126a1687e7a9
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-15 00:03:20 +0100

    Declared a pure component.

--- 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Notifications.css';
 
-class NotificationItem extends React.Component {
+class NotificationItem extends React.PureComponent {
   constructor(props) {
     super(props);
   }
