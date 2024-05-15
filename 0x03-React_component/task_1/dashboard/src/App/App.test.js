88c62471af8c992e11987986f0a0c856d0862e95
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-15 18:21:29 +0100

    Fixed error in task_1 notificationItem test cases

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -45,27 +45,38 @@ describe('App tests', () => {
   });
 });
 
-describe('App Component', () => {
-  let logOutMock, wrapper, alertMock;
-
-  beforeEach(() => {
-    logOutMock = jest.fn();
-    alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
-
-    wrapper = mount(<App logOut={logOutMock} />);
-  });
-
-  afterEach(() => {
-    alertMock.mockRestore();
-    wrapper.unmount();
-  });
-
-  it('calls logOut and shows alert when control + h is pressed', () => {
-    // Simulate pressing control + h
+describe('When ctrl + h is pressed', () => {
+  it('calls logOut function', () => {
+    const mocked = jest.fn();
+    const wrapper = mount(<App logOut={mocked} />);
     const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
     document.dispatchEvent(event);
 
-    expect(alertMock).toHaveBeenCalledWith('Logging you out');
-    expect(logOutMock).toHaveBeenCalled();
+    expect(mocked).toHaveBeenCalledTimes(1);
+    wrapper.unmount();
   });
+
+  document.alert = jest.fn();
+  it('checks that alert function is called', () => {
+    const wrapper = mount(<App />);
+    const spy = jest.spyOn(window, 'alert');
+    const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
+    document.dispatchEvent(event);
+
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+    wrapper.unmount();
+  });
+
+  it('checks that the alert is "Logging you out"', () => {
+    const wrapper = mount(<App />);
+    const spy = jest.spyOn(window, 'alert');
+    const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
+    document.dispatchEvent(event);
+
+    expect(spy).toHaveBeenCalledWith('Logging you out');
+    jest.restoreAllMocks();
+    wrapper.unmount();
+  });
+  document.alert.mockClear();
 });
