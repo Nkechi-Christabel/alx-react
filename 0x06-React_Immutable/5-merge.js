33a44b4c6d54ae3813cd496c658769b7004cac59
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-29 12:02:42 +0100

    Merged and concatenated.

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -7,6 +7,5 @@ export function concatElements(page1, page2) {
 
 // Function to merge the values of two objects into a List
 export function mergeElements(page1, page2) {
-  const mergedMap = Map(page1).merge(page2);
-  return List(mergedMap.toList());
+  return Map(page1).merge(page2);
 }
