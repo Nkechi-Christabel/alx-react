2fcb52f6d04d2a327a16d6e781ccfe187c59ba1c
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-30 00:51:22 +0100

    Updated the values of two objects deeply into a List.

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,5 +1,6 @@
 import { Map } from 'immutable';
 
+// Function to merge the values of two objects deeply into a List
 export default function mergeDeeplyElements(page1, page2) {
   return Map(page1).mergeDeep(Map(page2));
 }
