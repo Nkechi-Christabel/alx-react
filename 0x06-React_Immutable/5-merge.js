211e78eece87a4ec64060abef09d94e8741468db
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-05-30 09:45:14 +0100

    Added a function that prints best students with scores >= 70.

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,11 +1,11 @@
 import { List, Map } from 'immutable';
 
 // Function to concatenate the values of two arrays into a List
-export function concatElements(page1, page2) {
+export default function concatElements(page1, page2) {
   return List(page1.concat(page2));
 }
 
 // Function to merge the values of two objects into a List
-export function mergeElements(page1, page2) {
+export default function mergeElements(page1, page2) {
   return Map(page1).merge(page2);
 }
---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,20 @@
+import { Seq } from 'immutable';
+
+// Function to print best students with scores >= 70
+export default function printBestStudents(grades) {
+  const filteredGrades = Seq(grades)
+    .filter((student) => student.score >= 70)
+    .map((student) => ({
+      ...student,
+      firstName: capitalizeFirstLetter(student.firstName),
+      lastName: capitalizeFirstLetter(student.lastName),
+    }))
+    .toObject();
+
+  console.log(filteredGrades);
+}
+
+// Function to capitalize the first letter of a string
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
